9e86425da0788a040a2bac276e093f6318ba8050
김민식 <u1r2r3@naver.com>
2025-04-21 18:11:38 +0900

    feat: 직원 이력 도메인 정의 및 저장 구조 설계 #7
    - employeeNumber, memo, ipAddress 등 필터를 유연하게 조합하기 위해 Specification 사용

---
+++ src/main/java/com/hrbank/entity/EmployeeChangeLog.java
@@ -0,0 +1,50 @@
+package com.hrbank.entity;
+
+import com.hrbank.enums.EmployeeChangeLogType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeChangeLog {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  // 유형
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private EmployeeChangeLogType type;
+
+  // 사번
+  private String employeeNumber;
+
+  // 메모
+  private String memo;
+
+  // IP 주소
+  private String ipAddress;
+
+  // 시간
+  private LocalDateTime at;
+
+  // change_log_diffs, 변경 상세 정보 저장
+  @OneToMany(mappedBy = "changeLog", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<EmployeeChangeLogDetail> details = new ArrayList<>();
+}
---
+++ src/main/java/com/hrbank/entity/EmployeeChangeLogDetail.java
@@ -0,0 +1,35 @@
+package com.hrbank.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class EmployeeChangeLogDetail {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "change_log_id")
+  private EmployeeChangeLog changeLog;
+
+  @Column(name = "property_name")
+  private String propertyName;
+
+  private String before;
+
+  private String after;
+}
