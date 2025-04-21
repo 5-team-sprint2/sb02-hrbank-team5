6988312a3670a2926a2ef18807c663203331ed5f
Jaeseok Heo <jasonheo1998@gmail.com>
2025-04-21 16:38:34 +0900

    feat: Create Employee entity

---
+++ src/main/java/com/hrbank/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.hrbank.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Employee {
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @Column(unique = true, nullable = false)
+  private String employeeNumber;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "department_id")
+  private Department department;
+
+  @Column(nullable = false)
+  private String position;
+
+  @Column(nullable = false)
+  private LocalDate hireDate;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private EmployeeStatus status;
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "profile_image_id")
+  private BinaryContent profileImage;
+}
