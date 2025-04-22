9bde61908f2cfd70e8eb27804591539c5ba704ef
seonhyelin <seonseon933@gmail.com>
2025-04-22 11:46:56 +0900

    Feat: 파일 조회 및 다운로드 기능 추가.
    BasicBinaryContentService 패키지 위치 변경 및 빈 수정. BinaryContentService의 delete는 필요없는 것으로 보아 삭제.

--- src/main/java/com/hrbank/storage/BinaryContentStorage.java
+++ src/main/java/com/hrbank/storage/BinaryContentStorage.java
@@ -1,12 +1,19 @@
 package com.hrbank.storage;
 
+import com.hrbank.dto.binarycontent.BinaryContentDto;
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -29,6 +36,7 @@ public class BinaryContentStorage {
     }
   }
 
+  // 실제 파일 Storage에 저장
   public void put(Long id, byte[] data){
     Path filePath = root.resolve(id.toString());
     if (Files.exists(filePath)) {
@@ -39,6 +47,31 @@ public class BinaryContentStorage {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  // id로 실제 파일 찾기
+  public InputStream get(Long id){
+    Path filePath = root.resolve(id.toString());
+    if (Files.exists(filePath)) {
+      throw new RuntimeException("존재하지 않는 파일");
+    }
+    try {
+      return Files.newInputStream(filePath);
+    } catch (IOException e) {
+      throw new RuntimeException("파일 읽기 중 실패: " + e);
+    }
+  }
+
+  // 파일 다운로드
+  public ResponseEntity<Resource> download(BinaryContentDto contentDto) {
+    InputStream downloadData = get(contentDto.id());
+    Resource resource = new InputStreamResource(downloadData);
+
+    return ResponseEntity.ok()
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .header(HttpHeaders.CONTENT_DISPOSITION,
+            "attachment; filename=\"" + contentDto.fileName() + "\"")
+        .body(resource);
   }
 
 }
