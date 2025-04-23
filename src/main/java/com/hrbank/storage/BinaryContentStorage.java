b49d490ac216568fcefd0d2b7342bd4eede98c40
seonhyelin <seonseon933@gmail.com>
2025-04-23 10:03:42 +0900

    Refactor: 파일 다운로드 - 여러 컨텐트 타입으로 변환 가능하게 바꿈 & 도메인의 @Setter는 csv&log파일 생성 때 필요하여 추가.

--- src/main/java/com/hrbank/storage/BinaryContentStorage.java
+++ src/main/java/com/hrbank/storage/BinaryContentStorage.java
@@ -1,12 +1,15 @@
 package com.hrbank.storage;
 
 import com.hrbank.dto.binarycontent.BinaryContentDto;
+import com.hrbank.repository.EmployeeRepository;
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.InputStreamResource;
@@ -20,8 +23,10 @@ import org.springframework.stereotype.Component;
 @ConditionalOnProperty(name = "hrbank.storage.type", value = "local")
 public class BinaryContentStorage {
   private final Path root;
+  private static final Logger log = LoggerFactory.getLogger(BinaryContentStorage.class);
 
-  public BinaryContentStorage(@Value(".hrbank/storage") Path root){
+  public BinaryContentStorage(@Value(".hrbank/storage") Path root,
+      EmployeeRepository employeeRepository){
     this.root = root;
   }
 
@@ -52,7 +57,7 @@ public class BinaryContentStorage {
   // id로 실제 파일 찾기
   public InputStream get(Long id){
     Path filePath = root.resolve(id.toString());
-    if (Files.exists(filePath)) {
+    if (!Files.exists(filePath)) {
       throw new RuntimeException("존재하지 않는 파일");
     }
     try {
@@ -62,13 +67,27 @@ public class BinaryContentStorage {
     }
   }
 
-  // 파일 다운로드
+  // 파일 다운로드 - .csv, .log 컨텐트 타입 인식되도록 함.
   public ResponseEntity<Resource> download(BinaryContentDto contentDto) {
     InputStream downloadData = get(contentDto.id());
     Resource resource = new InputStreamResource(downloadData);
 
+    String contentTypeStr = contentDto.contentType();
+    MediaType mediaType;
+
+    if(contentTypeStr != null && !contentTypeStr.isBlank()) {
+      try{
+        mediaType = MediaType.parseMediaType(contentTypeStr);
+      }catch (Exception e){
+        log.warn("DTO에 저장된 contentType 값이 유효하지 않음: '{}'. application/octet-stream 으로 대체.", contentTypeStr, e);
+        mediaType = MediaType.APPLICATION_OCTET_STREAM;
+      }
+    }else {
+      mediaType = MediaType.APPLICATION_OCTET_STREAM;
+    }
+
     return ResponseEntity.ok()
-        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .contentType(mediaType)
         .header(HttpHeaders.CONTENT_DISPOSITION,
             "attachment; filename=\"" + contentDto.fileName() + "\"")
         .body(resource);
