6cd7cc8a0d30d0dcada2bf20803b6b30b890d775
Bokyung Kim <chloeee0228@gmail.com>
2025-04-24 10:19:23 +0900

    chore: 사용되지 않은 기존 메서드 삭제

--- src/main/java/com/hrbank/repository/DepartmentRepository.java
+++ src/main/java/com/hrbank/repository/DepartmentRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,13 +19,6 @@ public interface DepartmentRepository extends JpaRepository<Department, Long>, J
     @Query("SELECT d FROM Department d WHERE d.name = :name AND d.id <> :id")
     Optional<Department> findByNameExcludingId(@Param("name") String name, @Param("id") Long id);
 
-    List<Department> findByNameOrDescriptionWithSorting(
-            @Param("name") String name,
-            @Param("description") String description,
-            @Param("lastId") Long lastId,
-            @Param("limit") int limit,
-            @Param("sortField") String sortField,
-            @Param("sortDirection") String sortDirection);
 
     // 부서에 소속된 직원이 있는지 확인
     @Query("SELECT COUNT(e) > 0 FROM Employee e WHERE e.department.id = :departmentId")
