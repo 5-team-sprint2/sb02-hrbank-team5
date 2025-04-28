06b54872a7ec892b6c8f86aa4340a7450db4d31d
seonhyelin <seonseon933@gmail.com>
2025-04-28 18:04:16 +0900

    Chore: 서버 오류 발생하였을 때처럼 예시 작성

--- src/main/java/com/hrbank/controller/api/BinaryContentApi.java
+++ src/main/java/com/hrbank/controller/api/BinaryContentApi.java
@@ -1,9 +1,9 @@
 package com.hrbank.controller.api;
 
-import com.hrbank.dto.error.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -18,10 +18,18 @@ public interface BinaryContentApi {
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "다운로드 성공",
           content = @Content(mediaType = "*/*", schema = @Schema(type = "string", format = "binary"))),
-      @ApiResponse(responseCode = "404", description = "파일을 찾을 수 없음",
-          content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+      @ApiResponse(responseCode = "404", description = "잘못된 요청",
+          content = @Content(examples = @ExampleObject(
+              value = "{\"code\": \"BINARY_CONTENT_NOT_FOUND\", "
+                  + "\"status\": \"404\", "
+                  + "\"message\": \"프로필 이미지를 찾을 수 없습니다.\", "
+                  + "\"timestamp\": \"2025-03-06T05:39:06.152068Z\"}"))),
       @ApiResponse(responseCode = "500", description = "서버 오류",
-          content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+          content = @Content(examples = @ExampleObject(
+              value = "{\"code\": \"FILE_READ_ERROR\", "
+                  + "\"status\": \"500\", "
+                  + "\"message\": \"파일 읽기 중 오류가 발생했습니다.\", "
+                  + "\"timestamp\": \"2025-03-06T05:39:06.152068Z\"}")))
   })
   ResponseEntity<Resource> download(@Parameter(description = "파일 ID") Long id);
 }
