ba583e21d7c1e006834bca0d0864a570391c6565
seonhyelin <seonseon933@gmail.com>
2025-04-28 17:52:01 +0900

    Chore: api 에러 예시 통일되게 하지 않도록 변경

--- src/main/java/com/hrbank/dto/error/ErrorResponse.java
+++ src/main/java/com/hrbank/dto/error/ErrorResponse.java
@@ -5,24 +5,24 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "에러 응답")
 public record ErrorResponse(
-        @Schema(description = "에러 코드", example = "DEPARTMENT_NOT_FOUND")
-        String code,
-        @Schema(description = "HTTP 상태 코드", example = "400")
-        int status,
-        @Schema(description = "에러 메세지", example = "부서를 찾을 수 없습니다.")
-        String message,
-        @Schema(description = "에러 발생 시각", example = "2025-03-06T05:39:06.152068Z")
-        String timestamp,
-        @Schema(description = "상세 설명", example = "/departments/1")
-        String details
+    @Schema(description = "에러 코드")
+    String code,
+    @Schema(description = "HTTP 상태 코드")
+    int status,
+    @Schema(description = "에러 메세지")
+    String message,
+    @Schema(description = "에러 발생 시각")
+    String timestamp,
+    @Schema(description = "상세 설명")
+    String details
 ) {
-    public static ErrorResponse of(ErrorCode errorCode, String details, String timestamp) {
-        return new ErrorResponse(
-                errorCode.name(),
-                errorCode.getStatus().value(),
-                errorCode.getMessage(),
-                timestamp,
-                details
-        );
-    }
+  public static ErrorResponse of(ErrorCode errorCode, String details, String timestamp) {
+    return new ErrorResponse(
+        errorCode.name(),
+        errorCode.getStatus().value(),
+        errorCode.getMessage(),
+        timestamp,
+        details
+    );
+  }
 }
